85ff24fd43a13b0a900311ab84d7d3f1a49185df
ynonp <ynonperek@gmail.com>
2024-02-18 15:07:32 +0200

    add page

---
+++ gh-pages/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Blog To Telegram Github Page Demo</h1>
+      <p>This demo shows how to create a GitHub Page from a React app</p>
+    </>
+  )
+}
+
+export default App
